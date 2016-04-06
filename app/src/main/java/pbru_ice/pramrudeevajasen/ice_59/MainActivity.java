494ddd798d9192e05fa2213602585214d3c3f39b
PramrudeeOill <Pramrudee0141@gmail.com>
2016-04-06 23:12:14 +0700

    mainliststop4/6/59

--- app/src/main/java/pbru_ice/pramrudeevajasen/ice_59/MainActivity.java
+++ app/src/main/java/pbru_ice/pramrudeevajasen/ice_59/MainActivity.java
@@ -1,13 +1,40 @@
 package pbru_ice.pramrudeevajasen.ice_59;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //explicit
+    private Button AboutmeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+        buttoncontroller();
     }
-}
+
+    private void buttoncontroller() {
+        AboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.facebook.com/oil.dii.5"))
+            }
+        });
+    }
+
+    private void bindWidget() {
+        AboutmeButton = (Button) findViewById(R.id.button);
+    }
+    //bindwidget
+
+
+
+}//method
